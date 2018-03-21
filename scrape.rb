aa0eaaba49d1fcf9bbbf4d9b4bee4918141a6865
Dane Sowers <sowersdd@mail.uc.edu>
2018-03-21 15:44:46 -0400

    Added watir gem; updated scrape.rb to search Spotcrime site

--- scrape.rb
+++ scrape.rb
@@ -13,15 +13,17 @@ require 'mechanize'
 # Nokogiri gets the website data that could be read later on.
 require 'nokogiri'
 
+# Need Watir gem because there are AJAX requests on the SpotCrime page
+# NOTE: Make sure to install chromedriver 2.25 (http://chromedriver.storage.googleapis.com/index.html?path=2.25/)
+require 'watir'
+
 # resolv-replace.rb is more for testing, supplies nice error statements in case this script runs into network issues
 require 'resolv-replace.rb'
 require 'byebug'
 
-#HELLO WORLD!
-
 # Get yesterday's date
 # yesterday = Date.today.prev_day
-yesterday = Date.parse("February 6, 2018")
+yesterday = Date.parse("March 15, 2018")
 
 # Initialize new Mechanize agent
 agent = Mechanize.new
@@ -31,82 +33,62 @@ agent.read_timeout = 60
 # Chose Safari because I like Macs
 agent.user_agent_alias = "Mac Safari"
 
+browser = Watir::Browser.new
+
 # If website is down, we'll retry visiting it three times.
 retries = 0
 websiteDown = false
 
 # This array contains the districts we want to get crime info from
-districtArray = [ 'dis33', 'dis30', 'dis34', 'dis53', 'dis50', 'dis43', 'dis40', 'dis41', 'dis42', 'dis44' ]
+neighborhoodArray = [ 'clifton', 'corryville', 'cuf' ]
 
 
 mapURL = ""
 crimeNum = 0
 
-# for i in 0...districtArray.length
-# 	# Sleep between each request
-# 	sleep 4
-# 	begin
-# 		websiteURL = "http://www.columbuspolice.org/reports/Results?from=datePlaceholder&to=datePlaceholder&loc=locationPlaceholder&types=9"
-# 		# Insert search info into URL
-# 		websiteURL.gsub!('datePlaceholder', yesterday).gsub!('locationPlaceholder', districtArray[i])
-# 		# Try to direct to Columbus PD report website
-# 		page = agent.get websiteURL
-# 	# Rescue from HTTP GET request to CPD Site
-# 	rescue
-# 		if retries < 3
-# 			retries += 1
-# 			puts "Request #{retries} to CPD site failed, trying again"
-# 			sleep 5
-# 			retry
-# 		else
-# 			websiteDown = true
-# 			puts "CPD Site unavailable, skipping"
-# 			break
-# 		end
-# 	# Successful load of CPD site
-# 	else
-# 		resultPage = Nokogiri::HTML(page.body)
-
-# 		# Use this span class to find error
-# 		error_capture = resultPage.css("span[class='ErrorLabel']")
-
-# 		if error_capture.text.to_s.eql? "Your search produced no records."
-# 			puts "No crimes committed in CPD district #{districtArray[i]}"
-# 		else
-# 			cpd_elements = resultPage.css("table[class='mGrid']").css('td').css('tr')
-
-# 			# Get crime information
-# 			crimeNum = cpd_elements.length
-# 			i = 0
-# 			j = 0
-# 			linkIndex = 1
-
-# 			while ((i < crimeNum) && (i < 145)) do
-# 				report = '';
-# 				for j in 11...crimeReportNumbers[linkIndex]["onclick"].length - 1
-# 					char = '' + crimeReportNumbers[linkIndex]["onclick"][j]
-# 					report += char
-# 				end
-# 				# This loop takes care of setting up the links to each individual crime's page, where more information is listed.
-# 				puts "{"
-# 				puts "\tOffense: #{crimeInfo[i + 1].text}"
-# 				puts "\tVictim: #{crimeInfo[i + 2].text}"
-# 				puts "\tLocation: #{crimeInfo[i + 4].text}"
-# 				puts "\tReport Number #{report}"
-# 				puts "}"
-
-# 				i += 5
-# 				linkIndex += 1
-# 			end
-
-# 			# Crimes are retrieved from a table seperated by pages. Each page holds 29 crimes.
-# 			# JavaScript is used for pagination, and since Mechanize/Nokogiri cannot interact with JS (only HTML),
-# 			# the program can only retrieve the first 29 crimes (hence i < 145 [Each crime has 5 fields, 5 * 29 = 145])
-# 			# Until I figure out how to deal with pagination, we will only return the first 29 crimes.
-# 			# We rarely have more than 29 crimes, so this is a rare case, however it's something I still want to take care of.
-# 		end
-# 	end
-# end
+for i in 0...neighborhoodArray.length
+	# Sleep between each request
+	sleep 4
+	begin
+		websiteURL = "https://spotcrime.com/oh/cincinnati/locationPlaceholder"
+		# Insert search info into URL
+		websiteURL.gsub!('locationPlaceholder', neighborhoodArray[i])
+		# Try to direct to SpotCrime report website
+		browser.goto(websiteURL)
+	# Rescue from HTTP GET request to SpotCrime Site
+	rescue
+		if retries < 3
+			retries += 1
+			puts "Request #{retries} to SpotCrime site failed, trying again"
+			sleep 5
+			retry
+		else
+			websiteDown = true
+			puts "SpotCrime Site unavailable, skipping"
+			break
+		end
+	# Successful load of SpotCrime site
+	else
+		resultPage = Nokogiri::HTML(browser.html)
+		crime_blocks = resultPage.css("div.crime-list")
+		for j in 0...crime_blocks.length
+			crimes_in_block = crime_blocks[j].css("a")
+			for k in 0...crimes_in_block.length
+				crime_date = Date.strptime(crimes_in_block[k].css('.crime-date').text, '%m/%d/%Y')
+				if crime_date >= yesterday
+					puts "{"
+					puts "\tDetail Link: #{crimes_in_block[k]['href']}"
+					puts "\tCrime Type: #{crimes_in_block[k].css('h4').text}"
+					puts "\tDate: #{crime_date}"
+					puts "\tAddress #{crimes_in_block[k].css('.crime-address').text}"
+					puts "}"
+				else
+					break
+				end
+			end
+		end
+	end
+end
 
 websiteDown = false
 retries = 0
